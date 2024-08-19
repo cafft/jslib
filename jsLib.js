1ef9216df98eb6249dfbd313b351936ca55d2d94
cafft <csgtas@outlook.com>
2024-08-19 23:43:59 +0800

    Create jsLib.js

---
+++ jsLib.js
@@ -0,0 +1,39 @@
+var geTime = () => new Date().getTime(),
+    parse = x => JSON.parse(x),
+    encode = x => encodeURIComponent(x);
+
+function log(x) { 
+  this.java.log(x); 
+  return x; 
+}
+function t2s(x) { 
+  return String(this.java.t2s(x));
+}
+function s2t(x) { 
+  return String(this.java.s2t(x));
+}
+function setVar(x) { 
+  this.source.setVariable(String(x));
+}
+function getVar() { 
+  return String(this.source.getVariable()); 
+}
+function toast(x) { 
+  this.java.toast(x); 
+}
+function logToast(x) { 
+  this.java.log(x); 
+  this.java.toast(x); 
+}
+function getLoginMap(x) { 
+  try {
+     return String(this.source.getLoginInfoMap().get(x)); 
+  } catch {
+     this.java.log("获取loginMap失败!");
+     return '';
+  }
+} 
+function getFnInnerStr(fn){
+  let str = fn.toString();
+  return str.substring(str.indexOf('{') + 1, str.lastIndexOf('}'));
+}
